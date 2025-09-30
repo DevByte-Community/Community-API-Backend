b3e3a8cde1484a0ee98d4e62f7ee4f7d99ad3379
Sadiq Kabir <webemail655@gmail.com>
2025-09-30 15:33:52 +0100

    Removed the unused codes in the cofig and adjusted the port on my docker

--- config/config.js
+++ config/config.js
@@ -29,20 +29,3 @@ module.exports = {
     dialect: 'postgres',
   },
 }; 
-
-// require('dotenv').config();
-
-// module.exports = {
-//   development: {
-//     url: process.env.DATABASE_URL,
-//     dialect: 'postgres',
-//   },
-//   test: {
-//     url: process.env.DATABASE_URL,
-//     dialect: 'postgres',
-//   },
-//   production: {
-//     url: process.env.DATABASE_URL,
-//     dialect: 'postgres',
-//   },
-// };
