96d2262be57050e3b93fe34d614a5ef2181afca9
takamandre <andre@pluriel.tech>
2025-11-13 20:40:39 +0100

    bug: deploy with dev environment db configs updates

--- config/config.js
+++ config/config.js
@@ -10,13 +10,15 @@ dotenv.config({ path: path.resolve(process.cwd(), envFile) });
 
 module.exports = {
   development: {
-    username: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DB,
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
+    use_env_variable: 'DATABASE_URL', // Use connection string
     dialect: 'postgres',
     logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
   test: {
     username: process.env.POSTGRES_USER,
